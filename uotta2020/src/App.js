0739a92bfc8d75e23d698cc107e9a1fb55fc6ad5
Henry Morris <2001henrymorris@gmail.com>
2020-02-08 18:33:12 -0500

    asdasd

--- uotta2020/src/App.js
+++ uotta2020/src/App.js
@@ -3,14 +3,27 @@ import "firebase/auth";
 import LoginPage from "./components/pages/login/loginpage"
 import HomePage from './components/pages/home/HomePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
-    return (
-      <LoginPage />
-      //<Signup />
-      //<HomePage />
-    )
-  }
+  return (
+    <Router>
+      {/* <LoginPage /> */}
+      {/* //<Signup />
+        //<HomePage /> */}
+      <Switch>
+        <Route path='/login' component={LoginPage} />
+        <Route path='/home' component={HomePage} />
+        <Route component = {LoginPage} />
+      </Switch>
+    </Router>
+  )
+}
 
 
 export default App;
