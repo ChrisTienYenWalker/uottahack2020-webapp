22fd61afee23d7ae228fa8bf7fc25bc7dfeebb9f
Master4020 <47930042+Master4020@users.noreply.github.com>
2020-02-08 13:47:50 -0500

    finished sign in page

--- uotta2020/src/components/pages/login/loginpage.js
+++ uotta2020/src/components/pages/login/loginpage.js
@@ -3,88 +3,96 @@ import logo from './logo.png';
 import './App.css';
 import FirebaseApp from '../../../firebase.js';
 import "firebase/auth";
+import image from './image.jpg'
 
 var firebaseAuth = FirebaseApp.auth();
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = ({
-      textEmail: "",
-      textPassword: ""
-    });
-  }
+    constructor(props) {
+        super(props);
+        this.state = ({
+            textEmail: "",
+            textPassword: ""
+        });
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header ">
-          <div className="background">
-            <img src={logo} className="App-Logo" alt="logo" />
-            <p>UottawaHackathon2020</p>
-            <form className="forms" onKeyPress={ async (event) =>{
-              if(event.key == "Enter"){
-               var user = await firebaseAuth.signInWithEmailAndPassword(this.state.textEmail, this.state.textPassword).catch(function(error) {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                console.log(errorMessage)
-                // ...
-              });
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header ">
+                    <div className="background">
+                        <p>UottawaHackathon2020</p>
+                        <div style = {{display: 'flex'}}>
+                            <div className="left">
+                                <p>"He who has health has hope; and he who has hope has everything" - Arabian proverb</p>
+                            </div>
+                            <div className="right">
+                                <h1>Drug Time</h1>
+                                <img src={logo} className="App-Logo" alt="logo" />
+                                <form className="forms" onKeyPress={async (event) => {
+                                    if (event.key == "Enter") {
+                                        var user = await firebaseAuth.signInWithEmailAndPassword(this.state.textEmail, this.state.textPassword).catch(function (error) {
+                                            // Handle Errors here.
+                                            var errorCode = error.code;
+                                            var errorMessage = error.message;
+                                            console.log(errorMessage)
+                                            // ...
+                                        });
 
-                if (user != null) {
-                  console.log(user.user.email)
-                }
-                else {
-                  console.log("bad")
-                }
-              }
-            }
-            }>
-              <label>
-                Email:
+                                        if (user != null) {
+                                            console.log(user.user.email)
+                                        }
+                                        else {
+                                            console.log("bad")
+                                        }
+                                    }
+                                }
+                                }>
+                                    <label>
+                                        Email:
                 <input type="text" name="name" onChange={(event) => {
-                  this.setState({
-                    textEmail: event.target.value
-                  })
-                }} />
-                <br></br>
-              </label>
-              Password:
+                                            this.setState({
+                                                textEmail: event.target.value
+                                            })
+                                        }} />
+                                        <br></br>
+                                    </label>
+                                    Password:
                 <input type="text" name="password" onChange={(event) => {
-                this.setState({
-                  textPassword: event.target.value
-                })
-              }} />
-            </form>
-            <br></br>
-            <button
-              onClick={async () => {
-                var user = await firebaseAuth.signInWithEmailAndPassword(this.state.textEmail, this.state.textPassword).catch(function (error) {
-                  // Handle Errors here.
-                  var errorCode = error.code;
-                  var errorMessage = error.message;
-                  console.log(errorMessage)
-                  // ...
-                });
+                                        this.setState({
+                                            textPassword: event.target.value
+                                        })
+                                    }} />
+                                </form>
+                                <br></br>
+                                <button className="button"
+                                    onClick={async () => {
+                                        var user = await firebaseAuth.signInWithEmailAndPassword(this.state.textEmail, this.state.textPassword).catch(function (error) {
+                                            // Handle Errors here.
+                                            var errorCode = error.code;
+                                            var errorMessage = error.message;
+                                            console.log(errorMessage)
+                                            // ...
+                                        });
 
-                if (user != null) {
-                  console.log(user.user.email)
-                }
-                else {
-                  console.log("bad")
-                }
-              }
-              }>Log In</button>
-            <br></br>
-            <a href="#" className="subtext">Not signed up</a>
-            <br></br>
-            <a href="#" className="subtext">Forgot your password</a>
-          </div>
-        </header>
-      </div>
-    )
-  }
+                                        if (user != null) {
+                                            console.log(user.user.email)
+                                        }
+                                        else {
+                                            console.log("bad")
+                                        }
+                                    }
+                                    }>Log Into Your Most Important App</button>
+                                <br></br>
+                                <a href="#" className="subtext">Not signed up</a>
+                                <br></br>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+            </div>
+        )
+    }
 }
 
 
