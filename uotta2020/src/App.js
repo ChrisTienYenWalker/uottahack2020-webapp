d85fd493a6a7f6305dbdb464d5e12e42c1327e28
Master4020 <47930042+Master4020@users.noreply.github.com>
2020-02-08 11:29:05 -0500

    finished the login

--- uotta2020/src/App.js
+++ uotta2020/src/App.js
@@ -1,8 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import FirebaseApp from './firebase.js';
+import "firebase/auth";
 
-function App() {
+var firebaseAuth = FirebaseApp.auth();
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = ({
+      textEmail: "",
+      textPassword: ""
+    });
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="background">
+            <img src={logo} className= "App-Logo"alt="logo"/>
+            <p>UottawaHackathon2020</p>
+            <form  onKeyPress={ async (event) =>{
+              if(event.key == "Enter"){
+               var user = await firebaseAuth.signInWithEmailAndPassword(this.state.textEmail, this.state.textPassword).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorMessage)
+                // ...
+              });
+
+              if (user != null){
+                console.log(user.user.email)
+              }
+              else{
+                console.log("bad")
+              }
+              }
+            } 
+            }>
+              <label>
+                Email:
+                <input type="text" name="name" onChange={(event) => {
+                  this.setState({
+                    textEmail: event.target.value
+                  })
+                }}/>
+                <br></br>
+              </label>
+              Password:
+                <input type="text" name = "password" onChange={(event) => {
+                  this.setState({
+                    textPassword: event.target.value
+                  })
+                }}/>
+            </form>
+            <br></br>
+            <button
+              onClick={ async () => {
+              var user = await firebaseAuth.signInWithEmailAndPassword(this.state.textEmail, this.state.textPassword).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorMessage)
+                // ...
+              });
+
+              if (user != null){
+                console.log(user.user.email)
+              }
+              else{
+                console.log("bad")
+              }
+            }
+            }>Log In</button>
+            <br></br>            
+            <a href="#" className="subtext">Not signed up</a>
+            <br></br>
+            <a href="#" className="subtext">Forgot your password</a>
+            </div>
+        </header>
+      </div>
+    )
+  }
+}
+
+
+
+export default App;
+
+
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +111,5 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
-export default App;
---
+++ uotta2020/src/firebase.js
@@ -0,0 +1,16 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAANLMlWnz94p6QmRe3w6Hcp-y-shFY7ok",
+    authDomain: "uottahack2020.firebaseapp.com",
+    databaseURL: "https://uottahack2020.firebaseio.com",
+    projectId: "uottahack2020",
+    storageBucket: "uottahack2020.appspot.com",
+    messagingSenderId: "1068512406138",
+    appId: "1:1068512406138:web:d973d0331c20a0e51b29c3"
+  };
+
+const FirebaseApp = firebase.initializeApp(firebaseConfig);
+export default FirebaseApp
